43c206b7d1cd8aecd1f11b2ba73c390ff8309195
abdellah59 <abdellah.ait59@gmail.com>
2025-07-03 14:56:26 +0200

    feat(program.cs): add Main program loop for main meu

--- Brief-Duel-Guerriers/Program.cs
+++ Brief-Duel-Guerriers/Program.cs
@@ -6,45 +6,22 @@ namespace Brief_Duel_Guerriers
     {
         static void Main(string[] args)
         {
-            // Création des guerriers Gobelin et sorciere avec leur attribus et affichage de leur info au debut 
-            Gobelin Nico = new Gobelin("Nicolas", 40, 2, true);
-            Sorciere soso = new Sorciere("soso", 25, 5);
+            Console.WriteLine("----> DUELS DE GUERRIERS <----");
 
-            Console.WriteLine("Gbelin vs Sorciere :");
-            Nico.AfficherInfos();
-            soso.AfficherInfos();
-            Console.WriteLine();
+            // Boucle principale du programme:  permet d’afficher le menu principal tant que l’utilisateur ne choisit pas de quitter.
 
-            // Boucle du duel: tant que les deux personnages ont des points de vie continuer le duel
-
-            while (Nico.GetPointsDeVie() > 0 && soso.GetPointsDeVie() > 0)
+            bool continuerMenu = true;
+            while (continuerMenu)
             {
-                // Le gobelin attaque, et on récupère les dégâts infligés à la sorciere
-                int degatsGobelin = Nico.Attaquer();
-                soso.SubirDegats(degatsGobelin);
-                soso.AfficherInfos();
-
-                // Condition pour verifier si la sorcière est vaincue on arrete la boucle
-                if (soso.GetPointsDeVie() <= 0)
-                {
-                    Console.WriteLine($"{soso.GetNom()} est vaincu");
-                    break;
-                }
-
-                // La sorciere attaque a son tour, et on récupère les dégâts infligés au goblin
-                int degatsSorciere = soso.Attaquer();
-                Nico.SubirDegats(degatsSorciere);
-                Nico.AfficherInfos();
-
-                // Condition pour verifier si le gobelin n'a plus de pv alors il est vaincu et fin de la boucle
-                if (Nico.GetPointsDeVie() <= 0)
-                {
-                    Console.WriteLine($"{Nico.GetNom()} est vaincu");
-                    break;
-                }
+                continuerMenu = AfficherMenuPrincipal();
             }
 
+            Console.WriteLine("Fin de la partie");
 
+
+           
         }
+
     }
-};
+    
+}
