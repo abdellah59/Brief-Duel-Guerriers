ccd5de47b657bb1279f153d6e6b64d74622d239a
abdellah59 <abdellah.ait59@gmail.com>
2025-07-03 14:59:29 +0200

    feat(program.cs): add Method to Display the Main Menu

--- Brief-Duel-Guerriers/Program.cs
+++ Brief-Duel-Guerriers/Program.cs
@@ -18,8 +18,45 @@ namespace Brief_Duel_Guerriers
 
             Console.WriteLine("Fin de la partie");
 
+            // Methode pour Afficher le Menu Principal avec un switch pour avoir un menu avec des options 
 
-           
+            static bool AfficherMenuPrincipal()
+            {
+                Console.WriteLine("\n---> Menu Principal <---");
+                Console.WriteLine("1. Ajouter un guerrier");
+                Console.WriteLine("2. Afficher la listes des guerriers");
+                Console.WriteLine("3. Lancer un tournoi");
+                Console.WriteLine("4. Quitter");
+                Console.Write("Votre choix : ");
+
+                string choix = Console.ReadLine();
+
+                switch (choix)
+                {
+                    case "1":
+                        AjouterGuerrier();
+                        break;
+
+                    case "2":
+                        AfficherListeGuerriers();
+                        break;
+
+                    case "3":
+                        LancerTournoi();
+                        break;
+
+                    case "4": return false; // retourne false si l’utilisateur choisit de quitter et donc fin à la boucle.
+
+                    default:
+                        Console.WriteLine("Choix invalide. Veuillez réessayer avec 1,2,3 our 4."); // Affiche une erreur quand l'utlisateur fait un choix qui ne correspond pas aux options proposé
+                        break;
+
+                }
+
+                return true;
+
+            }
+
         }
 
     }
